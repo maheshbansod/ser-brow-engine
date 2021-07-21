749fb4fecfc9eeff45833d7167f8065e53d91a2c
Mahesh Bansod <mahesh0bansod@gmail.com>
2021-07-21 21:27:11 +0530

    created basic parser

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
 
-use std::collections::HashMap;
+// use std::collections::HashMap;
+
+use std::fs;
 
 mod dom;
+mod parser;
 
 fn main() {
-    let mut attrs: HashMap<String, String> = HashMap::new();
-    attrs.insert("bgcolor".to_string(),"skyblue".to_string());
-    let tree1 = dom::elem("html".to_string(), HashMap::new(),
-        vec![
-            dom::elem("head".to_string(), HashMap::new(),
-                vec![dom::elem("title".to_string(), HashMap::new(),
-                    vec![dom::text("Title of the page".to_string())])]),
-            dom::elem("body".to_string(), attrs,
-                vec![
-                        dom::elem("p".to_string(), HashMap::new(),
-                            vec![dom::text("A paragraph here lol".to_string())]),
-                        dom::comment("This is a comment. Above this, we've got a p element hmm".to_string())
-                    ])
-        ]);
+    // let mut attrs: HashMap<String, String> = HashMap::new();
+    // attrs.insert("bgcolor".to_string(),"skyblue".to_string());
+    // let tree1 = dom::elem("html".to_string(), HashMap::new(),
+    //     vec![
+    //         dom::elem("head".to_string(), HashMap::new(),
+    //             vec![dom::elem("title".to_string(), HashMap::new(),
+    //                 vec![dom::text("Title of the page".to_string())])]),
+    //         dom::elem("body".to_string(), attrs,
+    //             vec![
+    //                     dom::elem("p".to_string(), HashMap::new(),
+    //                         vec![dom::text("A paragraph here lol".to_string())]),
+    //                     dom::comment("This is a comment. Above this, we've got a p element hmm".to_string())
+    //                 ])
+    //     ]);
     
-    println!("{}", tree1);
+    // println!("{}", tree1);
+
+    println!("{}",parser::parse(fs::read_to_string("test.html").unwrap()));
 }
